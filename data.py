9231cccaa49157b287e8e5efeb05fbf9a4324c99
Kevin Velcich <kvelcich@gmail.com>
2017-05-30 11:44:01 -0700

    Adding shots on target

--- data.py
+++ data.py
@@ -37,46 +37,93 @@ for idx, row_idx in enumerate(range(1, sheet.nrows)):
 	home_length = len(fixture_data[home])
 	# HOME_ID,
 	fixture_sample.append(home)
+
+	# -- GOALS --
+	goals_scored = row[FT_GOALS_HOME].value
 	# HOME_AVG_GOALS,
 	if home_length == 0:
-		fixture_sample.append(row[FT_GOALS_HOME].value)
+		fixture_sample.append(goals_scored)
 	else:
 		prev_total = fixture_data[home][home_length - 1][HOME_SEASON_GOALS]
-		total = prev_total + row[FT_GOALS_HOME].value
+		total = prev_total + goals_scored
 		entry = total / (home_length + 1)
 		fixture_sample.append(entry)
 	# HOME_L1_GOALS,
-	fixture_sample.append(row[FT_GOALS_HOME].value)
+	fixture_sample.append(goals_scored)
 	# HOME_L5_GOALS,
 	if home_length == 0:
-		fixture_sample.append(row[FT_GOALS_HOME].value)
+		fixture_sample.append(goals_scored)
 	elif home_length < 5:
 		prev_total = fixture_data[home][home_length - 1][HOME_SEASON_GOALS]
-		entry = prev_total + row[FT_GOALS_HOME].value
+		entry = prev_total + goals_scored
 		fixture_sample.append(entry)
 	else:
 		l5_total = fixture_data[home][home_length - 1][HOME_L5_GOALS]
 		l5_game = fixture_data[home][home_length - 5][HOME_L1_GOALS]
-		entry = l5_total - l5_game + row[FT_GOALS_HOME].value
+		entry = l5_total - l5_game + goals_scored
 		fixture_sample.append(entry)
 	# HOME_L10_GOALS,
 	if home_length == 0:
-		fixture_sample.append(row[FT_GOALS_HOME].value)
+		fixture_sample.append(goals_scored)
 	elif home_length < 10:
 		prev_total = fixture_data[home][home_length - 1][HOME_SEASON_GOALS]
-		entry = prev_total + row[FT_GOALS_HOME].value
+		entry = prev_total + goals_scored
 		fixture_sample.append(entry)
 	else:
 		l10_total = fixture_data[home][home_length - 1][HOME_L10_GOALS]
 		l10_game = fixture_data[home][home_length - 10][HOME_L1_GOALS]
-		entry = l10_total - l10_game + row[FT_GOALS_HOME].value
+		entry = l10_total - l10_game + goals_scored
 		fixture_sample.append(entry)
 	# HOME_SEASON_GOALS,
 	if home_length == 0:
-		fixture_sample.append(row[FT_GOALS_HOME].value)
+		fixture_sample.append(goals_scored)
 	else:
 		prev_total = fixture_data[home][home_length - 1][HOME_SEASON_GOALS]
-		entry = prev_total + row[FT_GOALS_HOME].value
+		entry = prev_total + goals_scored
+		fixture_sample.append(entry)
+
+	# -- SHOTS ON --
+	shots_on = row[SHOTS_TARGET_HOME].value
+	# HOME_AVG_SHOTS_ON,
+	if home_length == 0:
+		fixture_sample.append(shots_on)
+	else:
+		prev_total = fixture_data[home][home_length - 1][HOME_SEASON_SHOTS_ON]
+		total = prev_total + shots_on
+		entry = total / (home_length + 1)
+		fixture_sample.append(entry)
+	# HOME_L1_SHOTS_ON,
+	fixture_sample.append(shots_on)
+	# HOME_L5_SHOTS_ON,
+	if home_length == 0:
+		fixture_sample.append(shots_on)
+	elif home_length < 5:
+		prev_total = fixture_data[home][home_length - 1][HOME_SEASON_SHOTS_ON]
+		entry = prev_total + shots_on
+		fixture_sample.append(entry)
+	else:
+		l5_total = fixture_data[home][home_length - 1][HOME_L5_SHOTS_ON]
+		l5_game = fixture_data[home][home_length - 5][HOME_L1_SHOTS_ON]
+		entry = l5_total - l5_game + shots_on
+		fixture_sample.append(entry)
+	# HOME_L10_SHOTS_ON,
+	if home_length == 0:
+		fixture_sample.append(shots_on)
+	elif home_length < 10:
+		prev_total = fixture_data[home][home_length - 1][HOME_SEASON_SHOTS_ON]
+		entry = prev_total + shots_on
+		fixture_sample.append(entry)
+	else:
+		l10_total = fixture_data[home][home_length - 1][HOME_L10_SHOTS_ON]
+		l10_game = fixture_data[home][home_length - 10][HOME_L1_SHOTS_ON]
+		entry = l10_total - l10_game + shots_on
+		fixture_sample.append(entry)
+	# HOME_SEASON_SHOTS_ON,
+	if home_length == 0:
+		fixture_sample.append(shots_on)
+	else:
+		prev_total = fixture_data[home][home_length - 1][HOME_SEASON_SHOTS_ON]
+		entry = prev_total + shots_on
 		fixture_sample.append(entry)
 
 	print idx, ' ', fixture_sample

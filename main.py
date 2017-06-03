3de83ab35f4f3c3ee29490d32fabfab16d3ac989
Kevin Velcich <kvelcich@gmail.com>
2017-06-03 16:36:12 -0700

    Fixing run error

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ sum = 0
 print 'Scraping data'
 for sheet in List.sheets:
 	print sheet
-	data = Scraper.scrape('Data/spreadsheets/all-euro-data-' + sheet + '.xls')
+	data = Scraper.scrape('Data/Spreadsheets/all-euro-data-' + sheet + '.xls')
 	for i in range(len(data)):
 		sum += len(data[i])
 sum /= 2
